0704241e8aa2941bb3e3931005d3f6129953c7da
romabliski <jakub.romaszewski@gmail.com>
2018-05-29 00:03:42 +0200

    extended interface

--- PayU.Wrapper/Src/PayU.Wrapper.Client/IPayUClient.cs
+++ PayU.Wrapper/Src/PayU.Wrapper.Client/IPayUClient.cs
@@ -1,7 +1,16 @@
-﻿namespace PayU.Wrapper.Client
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace PayU.Wrapper.Client
 {
     public interface IPayUClient
     {
-        
+        /// <summary>
+        /// Gets the connection to API.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <returns>Response</returns>
+        Task<IRestResponse> PostOrder();
     }
 }
--- PayU.Wrapper/Src/PayU.Wrapper.Client/IRequestBuilder.cs
+++ PayU.Wrapper/Src/PayU.Wrapper.Client/IRequestBuilder.cs
@@ -1,10 +1,14 @@
-﻿namespace PayU.Wrapper.Client
+﻿using System.Threading.Tasks;
+using PayU.Wrapper.Client.Data;
+using RestSharp;
+
+namespace PayU.Wrapper.Client
 {
     /// <summary>
     /// Request Builder Interface
     /// </summary>
     public interface IRequestBuilder
     {
-        
+        Task<IRestRequest> PostOrders();
     }
 }
