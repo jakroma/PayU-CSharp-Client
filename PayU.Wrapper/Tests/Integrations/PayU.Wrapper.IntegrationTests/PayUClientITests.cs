277d359b180d0a57d9f03754ef71574787687888
romabliski <jakub.romaszewski@gmail.com>
2018-05-29 00:04:42 +0200

    Test extended

---
+++ PayU.Wrapper/Tests/Integrations/PayU.Wrapper.IntegrationTests/PayUClientITests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using PayU.Wrapper.Client;
+using PayU.Wrapper.Client.Data;
+using RestSharp;
+
+namespace PayU.Wrapper.IntegrationTests
+{
+    [TestFixture]
+    public class PayUClientITests
+    {
+        /// <summary>
+        /// The base URL
+        /// </summary>
+        private readonly string baseUrl = "";
+
+        public void PostPayment_WhenCall_ResultExpected()
+        {
+            //Arrange
+            PayUClient payUClient = new PayUClient(baseUrl);
+            PaymentContract payment = new PaymentContract
+            {
+                //TODO
+            };
+
+            //Act
+            IRestResponse act = payUClient.PostOrder().Result;
+
+            //Assert
+            Console.WriteLine(act);
+        }
+    }
+}
