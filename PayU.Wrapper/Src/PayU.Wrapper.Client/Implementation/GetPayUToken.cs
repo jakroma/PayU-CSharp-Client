f60b9f1aa3cd305fbd55ce38d62d57426f229ef9
romabliski <jakub.romaszewski@gmail.com>
2018-06-21 18:40:04 +0200

    Reconstruct architecture add GetPayUToken

---
+++ PayU.Wrapper/Src/PayU.Wrapper.Client/Implementation/GetPayUToken.cs
@@ -0,0 +1,49 @@
+﻿using PayU.Wrapper.Client.Data;
+
+namespace PayU.Wrapper.Client
+{
+    public class GetPayUToken
+    {
+        /// <summary>
+        /// base url to connect with API (Production or Test Server)
+        /// </summary>
+        private string _baseUrl;
+
+        /// <summary>
+        /// The PayU client
+        /// </summary>
+        private IPayUClient _payUClient;
+
+        /// <summary>
+        /// The user request
+        /// </summary>
+        private UserRequest _userRequest;
+
+        /// <summary>
+        /// The token contract
+        /// </summary>
+        private TokenContract _tokenContract;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetPayUToken"/> class.
+        /// </summary>
+        /// <param name="isProduction">if set to <c>true</c> [you want request production server].</param>
+        /// <param name="userRequest">The user request.</param>
+        public GetPayUToken(bool isProduction, UserRequest userRequest)
+        {
+            _userRequest = userRequest;
+            _baseUrl = isProduction ? "https://secure.payu.com" : "https://secure.snd.payu.com";
+            _payUClient = new PayUClient(userRequest, _baseUrl);
+            _tokenContract = _payUClient.Request<TokenContract>(Enum.PayURequestType.PostAOuthToken).Result;
+        }
+
+        /// <summary>
+        /// Tokens the return.
+        /// </summary>
+        /// <returns>PayU Token Class</returns>
+        public IPayUClient TokenReturn()
+        {
+            return _payUClient;
+        }
+    }
+}
---
+++ PayU.Wrapper/Src/PayU.Wrapper.Client/Implementation/PayUClient.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using PayU.Wrapper.Client.Data;
+using PayU.Wrapper.Client.Enum;
+using PayU.Wrapper.Client.Exception;
+
+namespace PayU.Wrapper.Client
+{
+    /// <summary>
+    /// Pay U Client Class
+    /// </summary>
+    /// <seealso cref="PayU.Wrapper.Client.IPayUClient" />
+    public class PayUClient : IPayUClient
+    {
+        /// <summary>
+        /// The token
+        /// </summary>
+        private TokenContract _tokenContract;
+
+        /// <summary>
+        /// The user request
+        /// </summary>
+        private UserRequest _userRequest;
+
+        /// <summary>
+        /// The rest builder
+        /// </summary>
+        private readonly IRestBuilder _restBuilder;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PayUClient"/> class.
+        /// </summary>
+        /// <param name="isProducition">if set to <c>true</c> [is producition].</param>
+        /// <param name="baseUrl">base url to connect with API</param>
+        public PayUClient(UserRequest userRequest, string baseUrl)
+         {
+             _userRequest = userRequest;
+             _restBuilder = new RestBuilder(baseUrl);
+        }
+
+        public async Task<T> Request<T>(PayURequestType payURequestType)
+        {
+            try
+            {
+            if (_tokenContract == null)
+            {
+                _tokenContract = await _restBuilder.PostAOuthToken<TokenContract>(_userRequest);
+            }
+            switch (payURequestType)
+            {
+                case PayURequestType.GetOrderDetails:
+                    return (T)Convert.ChangeType(_restBuilder.GetOrderDetails<T>(_userRequest.DataToRequest.OrderId, _tokenContract) , typeof(T));
+
+                case PayURequestType.GetRefundOrder:
+                    return (T)Convert.ChangeType(_restBuilder.GetRefundOrder<T>(_userRequest.DataToRequest.OrderId, _tokenContract) , typeof(T));
+
+                //case PayURequestType.UpdateOrder:
+                //    return this;
+
+                //case PayURequestType.CancelOrder:
+                //    return this;
+
+                case PayURequestType.PostCreateNewOrder:
+                    return (T)Convert.ChangeType(_restBuilder.PostCreateNewOrder<T>(_userRequest.DataToRequest.OrderId, _tokenContract, _userRequest.DataToRequest.OrderContract), typeof(T)); ;
+
+                //case PayURequestType.PayOutFromShop:
+                //    return this;
+
+                //case PayURequestType.RetrevePayout:
+                //    return this;
+
+                //case PayURequestType.FinishRequest:
+                //    return this;
+
+                default:
+                    throw new InvalidRequestType();
+
+            }
+        }
+            catch (AggregateException innerException)
+            {
+                Console.WriteLine($"{innerException.Message}");
+                throw innerException.InnerException.InnerException;
+            }
+        }
+    }
+}
